6324b1a328fd254824bbf12fa9caefa2776a2e3f
Seth Cost <scost46@gmail.com>
2023-02-11 12:08:51 -0600

    Finished all questions, checking work

--- scripts/movieproject.sql
+++ scripts/movieproject.sql
@@ -41,12 +41,16 @@ GROUP BY DISTINCT(company_name)
 ORDER BY number_of_films DESC;
 
 --5. Write a query that returns the five distributors with the highest average movie budget.
-SELECT *
+SELECT DISTINCT(company_name), ROUND(AVG(film_budget),2) AS avg_budget
 FROM distributors
-LIMIT 5
+INNER JOIN specs
+ON distributors.distributor_id = specs.domestic_distributor_id
+INNER JOIN revenue
+ON specs.movie_id = revenue.movie_id
+GROUP BY company_name
+ORDER BY avg_budget DESC
+LIMIT 5;
 
-SELECT AVG(film_budget)
-FROM revenue
 
 --6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
 SELECT company_name, headquarters, film_title, imdb_rating
@@ -69,5 +73,5 @@ SELECT ROUND(AVG(imdb_rating),2) AS avg_rating
 FROM specs
 INNER JOIN rating
 USING(movie_id)
-WHERE length_in_min < 120
+WHERE length_in_min < 120;
 -- over two hours
